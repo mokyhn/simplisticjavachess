4532c8986410b8468bd217c39b5d0c870adb7d62
mokyhn@gmail.com <mokyhn@gmail.com@6e7f4ec5-5eba-651d-5b14-7bd2705824ce>
2013-06-29 20:27:57 +0000

    Experiments in alpha beta search

--- trunk/src/sjc/AlphaBetaSearch.java
+++ trunk/src/sjc/AlphaBetaSearch.java
@@ -9,7 +9,8 @@ package sjc;
 import java.util.ArrayList;
 
 public class AlphaBetaSearch extends AbstractSearch {
-        private int alphaBetaSearch(int currentPlyDepth, int totalPlyDepth, int alpha, int beta) {
+        
+    private int alphaBetaSearchOld(int currentPlyDepth, int totalPlyDepth, int alpha, int beta) {
         ArrayList<Move> moves;
         Move            m;
         int             evaluation;
@@ -54,7 +55,7 @@ public class AlphaBetaSearch extends AbstractSearch {
         } */
         evaluation = 0;
         int i;
-        for (i = 0; (i < moves.size() && alpha < beta ); i++) {  
+        for (i = 0; (i < moves.size() && alpha < beta ); i++) {  // TODO: Is this condition right?
             m = moves.get(i);
             /*if (m.toString().contains("a5") && m.toString().contains("a6")) {
              System.out.println("WHITE: Eval, alpha = " + evaluation + ", " + alpha + " " + m.toString());
@@ -125,7 +126,121 @@ public class AlphaBetaSearch extends AbstractSearch {
         return inMove == Piece.WHITE ? alpha : beta;
     }
 
+    //TODO: Rework this algorithm. The algorithm above is the "old" almost working...
+    private int alphaBetaSearch(int currentPlyDepth, int totalPlyDepth, int alpha, int beta) {
+        Boolean         thereWasALegalMove = false;
+        final int       inMove = analyzeBoard.inMove();
+        int             distanceToRoot = totalPlyDepth - currentPlyDepth;
         
+        // Assertions
+        assert(currentPlyDepth <= totalPlyDepth);
+        assert(currentPlyDepth >= 0 && totalPlyDepth >= 0);
+        
+        // If the game has ended return immediately.
+        if (analyzeBoard.isDraw()) 
+        {
+            return 0;
+        }
+   
+        if (analyzeBoard.isMate()) 
+        {
+            if (inMove == Piece.WHITE) 
+            {
+                return Evaluator.WHITE_IS_MATED + distanceToRoot;
+            }
+            else 
+            {
+                return Evaluator.BLACK_IS_MATED - distanceToRoot;
+            }
+        }
+        
+        // Test for other kinds of draw.
+        if (analyzeBoard.drawBy50MoveRule() || analyzeBoard.drawBy3RepetionsRule()) {
+             analyzeBoard.setDraw();
+             return 0;
+        }
+        
+        if (currentPlyDepth == 0) {
+            noPositions++;
+            return Evaluator.evaluate(analyzeBoard);
+        }
+                
+        ArrayList<Move> possibleMoves = Movegenerator.generateAllMoves(analyzeBoard);
+
+        int i;
+        for (i = 0; (i < possibleMoves.size() && alpha < beta ); i++) {  // TODO: Is this condition right?
+            Move m = possibleMoves.get(i);
+            /*if (m.toString().contains("a5") && m.toString().contains("a6")) {
+             System.out.println("WHITE: Eval, alpha = " + evaluation + ", " + alpha + " " + m.toString());
+            }*/
+            analyzeBoard.performMove(m);
+            if (analyzeBoard.isInCheck(m.whoMoves)) {
+              analyzeBoard.retractMove(); // The move was not legal
+              continue;                   // Try next pseudolegal move
+            }
+            thereWasALegalMove = true;
+            
+            //System.out.print("(" + m.toString());
+            int variantEvaluation = alphaBetaSearch(currentPlyDepth-1, totalPlyDepth, alpha, beta);
+            //System.out.println(")");
+            /* if (m.toString().contains("a5") && m.toString().contains("a6")) {
+             System.out.println("WHITE: Eval, alpha = " + alpha +
+                                " beta = " + beta +
+                                " currentDepth " + currentPlyDepth +
+                                " evaluation = " + evaluation + " " +
+                                " move " + m.toString() +
+                                analyzeBoard.toString() + "\n\n\n\n");
+            }*/
+            analyzeBoard.retractMove();
+            
+            if (inMove == Piece.WHITE) {               
+                if (variantEvaluation > alpha) {
+                 alpha = variantEvaluation;
+                 if (currentPlyDepth == totalPlyDepth)  {
+                     //System.out.println("WHITE: Eval, alpha = " + eval + ", " + alpha + " " + m.toString());
+                     strongestMove = m;
+                 }
+                }
+            } else {
+             if (variantEvaluation < beta) {
+                 if (currentPlyDepth == totalPlyDepth)  {
+                             //if (m.toString().contains("e5")) { 
+                             //    System.out.printf("\nBLACK: move = %s, Eval=%d, alpha=%d, beta=%d, sd = %d\n", m.toString(), eval , alpha, beta, currentPlyDepth);
+                            // }
+                             //if (m.toString().contains("h6") ) { 
+                             //    System.out.printf("\nBLACK: move = %s, Eval=%d, alpha=%d, beta=%d, sd = %d\n", m.toString(), eval , alpha, beta, currentPlyDepth);
+                             //}
+                             strongestMove = m;
+                         }        
+                 beta = variantEvaluation;
+             }
+            }    
+        }
+        
+        // For the purpose of statistics
+        if (alpha >=  beta) {
+                wastedGeneratedMoves = wastedGeneratedMoves + (possibleMoves.size()) - i;
+                noCutOffs++;
+        }
+        
+        // Mate or draw
+        if (!thereWasALegalMove) {
+           if (analyzeBoard.isInCheck(inMove)) {
+            analyzeBoard.setMate();            
+            //System.out.println("Matefound:\n" + analyzeBoard.toString());
+            if (inMove == Piece.WHITE) return Evaluator.WHITE_IS_MATED+distanceToRoot;
+            else return Evaluator.BLACK_IS_MATED-distanceToRoot;
+           } else {
+               analyzeBoard.setDraw();
+               return 0;
+           } // draw
+         }
+        
+        return inMove == Piece.WHITE ? alpha : beta;
+    }
+
+    
+    
     protected final int search() {
       System.out.println("Alpha-Beta search...");
       return alphaBetaSearch(_plyDepth, _plyDepth, Integer.MIN_VALUE, Integer.MAX_VALUE);
