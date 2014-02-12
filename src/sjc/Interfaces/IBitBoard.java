155fd1a5d6557ab0c254af065cd3e9ac52f26658
mokyhn <mokyhn@gmail.com>
2014-02-12 17:24:49 +0000

    Added interface definition to Bitboard

---
+++ src/sjc/Interfaces/IBitBoard.java
@@ -0,0 +1,17 @@
+package sjc.Interfaces;
+
+import sjc.Piece;
+
+/**
+ *
+ * @author Morten Kühnrich
+ * @year 2014
+ */
+public interface IBitBoard extends Cloneable{
+  public void insertPiece(Piece p);
+  public boolean hasPiece(int x, int y, int color, int type);
+  public Piece removePiece(int x, int y);
+  @Override public String toString();
+  @Override public boolean equals(Object obj);
+  public IBitBoard clone();
+}
