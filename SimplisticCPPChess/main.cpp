3ae4d420852ae38f58cbaf76091fd9923b3b7532
mokyhn <mokyhn@6e7f4ec5-5eba-651d-5b14-7bd2705824ce>
2014-02-20 07:50:06 +0000

    Adding C++ project

---
+++ SimplisticCPPChess/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: mshome
+ *
+ * Created on 20. februar 2014, 08:47
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
