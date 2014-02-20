3b67e3e5912930c7642592e3f44c55e131bab5bd
mokyhn <mokyhn@6e7f4ec5-5eba-651d-5b14-7bd2705824ce>
2014-02-20 20:32:52 +0000

    Starting c++

--- SimplisticCPPChess/main.cpp
+++ SimplisticCPPChess/main.cpp
@@ -18,7 +18,7 @@ using namespace std;
  */
 int main(int argc, char** argv) {
     cout << "Hello world!" << endl;
-    cout << getIndexFromColor(PieceColor.WHITE);
+    cout << (getIndexFromColor(PieceColor::WHITE) == 1);
     return 0;
 }
 
