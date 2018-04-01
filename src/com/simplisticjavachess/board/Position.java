f3f6a0fdcfa522dd5b5b5ee9410793bc552ec9ca
mokyhn@gmail.com <Familien Khnrich@192.168.1.54>
2018-04-01 23:58:33 +0200

    Hashing is done with Objects.hash now

--- src/com/simplisticjavachess/board/Position.java
+++ src/com/simplisticjavachess/board/Position.java
@@ -9,6 +9,7 @@ import com.simplisticjavachess.piece.Piece;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class Position
 {
@@ -133,8 +134,12 @@ public class Position
             return false;
         }
     }
-    
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(piecesMap.values());
+    }
+    
     public BitBoard getBitBoard()
     {
         return bitBoard;
