87e958480855f7673c036c847f254e754b50f996
mokyhn@gmail.com <Familien Khnrich@192.168.1.54>
2018-04-07 23:13:38 +0200

    Refactoring

--- src/com/simplisticjavachess/movegenerator/MoveGeneratorUtil.java
+++ src/com/simplisticjavachess/movegenerator/MoveGeneratorUtil.java
@@ -6,6 +6,7 @@
 package com.simplisticjavachess.movegenerator;
 
 import com.simplisticjavachess.board.Board;
+import com.simplisticjavachess.board.Location;
 import com.simplisticjavachess.move.Move;
 import com.simplisticjavachess.move.MoveType;
 import com.simplisticjavachess.piece.Piece;
@@ -16,29 +17,34 @@ public class MoveGeneratorUtil
     // Used for generation of knight, bishop, rook and queen moves
     public static Move genMove(Board board, Piece fp, int dX, int dY)
     {
+        Location from = fp.getLocation();
+        
         final int tX = fp.getxPos() + dX;
         final int tY = fp.getyPos() + dY;
+        
         MoveType moveType;
         assert fp.getColor() == board.inMove();
-        if (fp.getxPos() < 0 || fp.getxPos() > 7 || fp.getyPos() < 0 || fp.getyPos() > 7 || tX < 0 || tX > 7 || tY < 0 || tY > 7)
+        if (tX < 0 || tX > 7 || tY < 0 || tY > 7)
         {
             return null;
         }
-        Piece takenPiece = board.getPiece(tX, tY);
 
+        Location to = new Location(tX, tY);
+        Piece takenPiece = board.getPiece(to);
+        
         Move move = null;
         if (takenPiece != null && takenPiece.getColor() == board.inMove().opponent())
         {
             moveType = MoveType.CAPTURE;
-            move = new Move(fp.getxPos(), fp.getyPos(), tX, tY, moveType, takenPiece, board.inMove());
+            move = new Move(from, to, moveType, takenPiece, board.inMove());
         } 
         else
         {
-            if (board.freeSquare(tX, tY))
+            if (board.freeSquare(to))
             {
                 takenPiece = null;
                 moveType = MoveType.NORMALMOVE;
-                move = new Move(fp.getxPos(), fp.getyPos(), tX, tY, moveType, takenPiece, board.inMove());
+                move = new Move(from, to, moveType, takenPiece, board.inMove());
             }
         }
         return move;
