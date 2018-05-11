6bb852ce17e1d423b79a729e170abb18cafd111d
mokyhn@gmail.com <Familien Khnrich@192.168.1.54>
2018-05-11 15:58:58 +0200

    Added move sequence

---
+++ src/com/simplisticjavachess/search/MoveSequence.java
@@ -0,0 +1,14 @@
+
+package com.simplisticjavachess.search;
+
+import com.simplisticjavachess.misc.ImmutableList;
+import com.simplisticjavachess.move.Move;
+
+/**
+ *
+ * @author Morten Kühnrich
+ */
+public class MoveSequence extends ImmutableList<Move>
+{
+    
+}
