87ae9a8bf4358e64194d3859e1c98dc021918999
mokyhn <mokyhn@gmail.com>
2019-05-03 19:36:45 +0200

    Fixing some broken tests.

--- src/test/java/com/simplisticjavachess/board/BoardTest_Castling.java
+++ src/test/java/com/simplisticjavachess/board/BoardTest_Castling.java
@@ -10,18 +10,30 @@ import static org.junit.Assert.*;
 import org.junit.Ignore;
 
 public class BoardTest_Castling
-{  
+{
+    private static Board WHITE_IN_MOVE_CASTLING() { return Board.createFromFEN("r3k2r/8/8/8/8/8/8/R3K2R w KQkq"); }
+    private static Board WHITE_IN_MOVE_CANNOT_CASTLE() { return Board.createFromFEN("r3k2r/8/8/8/8/8/8/R3K2R w kq"); }
+    private static Board AFTER_WHITE_SHORT_CASTLING() {return Board.createFromFEN("r3k2r/8/8/8/8/8/8/R4RK1 b kq"); }
+    private static Board AFTER_WHITE_LONG_CASTLING() {return Board.createFromFEN("r3k2r/8/8/8/8/8/8/2KR3R b kq"); }
+
+
+    private static Board BLACK_IN_MOVE_CASTLING() { return Board.createFromFEN("r3k2r/8/8/8/8/8/8/R3K2R b KQkq"); }
+    private static Board BLACK_IN_MOVE_CANNOT_CASTLE() { return Board.createFromFEN("r3k2r/8/8/8/8/8/8/R3K2R b KQ"); }
+    private static Board AFTER_BLACK_SHORT_CASTLING() {return Board.createFromFEN("r4rk1/8/8/8/8/8/8/R3K2R w kq"); }
+    private static Board AFTER_BLACK_LONG_CASTLING() {return Board.createFromFEN("2kr3r/8/8/8/8/8/8/R3K2R w kq"); }
+
+
     @Test
     public void testWhiteShortCastling()
     {
         // Test of doMove
-        Board actual = new Board(Boards.WHITE_IN_MOVE_CASTLING());
-        Board expected = Boards.AFTER_WHITE_SHORT_CASTLING();        
+        Board actual = new Board(WHITE_IN_MOVE_CASTLING());
+        Board expected = AFTER_WHITE_SHORT_CASTLING();
         actual.doMove(Moves.WHITE_SHORT_CASTLING());
-        assertEquals(expected, actual);
+        assertEquals(expected.getPosition(), actual.getPosition());
 
         // Test of undo
-        expected = Boards.WHITE_IN_MOVE_CASTLING();
+        expected = WHITE_IN_MOVE_CASTLING();
         actual.undo();
         assertEquals(expected, actual);                
     }
@@ -31,23 +43,23 @@ public class BoardTest_Castling
     public void testWhiteShortCastling_cannotCastle()
     {
         // Test of doMove
-        Board actual = new Board(Boards.WHITE_IN_MOVE_CANNOT_CASTLE());
+        Board actual = new Board(WHITE_IN_MOVE_CANNOT_CASTLE());
         boolean result = actual.doMove(Moves.WHITE_SHORT_CASTLING());
         assertFalse(result);
-        assertEquals(actual, Boards.WHITE_IN_MOVE_CANNOT_CASTLE() );
+        assertEquals(actual, WHITE_IN_MOVE_CANNOT_CASTLE() );
     }
     
     @Test
     public void testBlackShortCastling()
     {
         // Test of doMove
-        Board actual = new Board(Boards.BLACK_IN_MOVE_CASTLING());
-        Board expected = Boards.AFTER_BLACK_SHORT_CASTLING();        
+        Board actual = new Board(BLACK_IN_MOVE_CASTLING());
+        Board expected = AFTER_BLACK_SHORT_CASTLING();
         actual.doMove(Moves.BLACK_SHORT_CASTLING());
-        assertEquals(expected, actual);
+        assertEquals(expected.getPosition(), actual.getPosition());
 
         // Test of undo
-        expected = Boards.BLACK_IN_MOVE_CASTLING();
+        expected = BLACK_IN_MOVE_CASTLING();
         actual.undo();
         assertEquals(expected, actual);                
     }
@@ -57,23 +69,23 @@ public class BoardTest_Castling
     public void testBlackShortCastling_cannotCastle()
     {
         // Test of doMove
-        Board actual = new Board(Boards.BLACK_IN_MOVE_CANNOT_CASTLE());
+        Board actual = new Board(BLACK_IN_MOVE_CANNOT_CASTLE());
         boolean result = actual.doMove(Moves.BLACK_SHORT_CASTLING());
         assertFalse(result);
-        assertEquals(actual, Boards.BLACK_IN_MOVE_CANNOT_CASTLE() );
+        assertEquals(actual, BLACK_IN_MOVE_CANNOT_CASTLE() );
     }
 
     @Test
     public void testLongShortCastling()
     {
         // Test of doMove
-        Board actual = new Board(Boards.WHITE_IN_MOVE_CASTLING());
-        Board expected = Boards.AFTER_WHITE_LONG_CASTLING();        
+        Board actual = new Board(WHITE_IN_MOVE_CASTLING());
+        Board expected = AFTER_WHITE_LONG_CASTLING();
         actual.doMove(Moves.WHITE_LONG_CASTLING());
-        assertEquals(expected, actual);
+        assertEquals(expected.getPosition(), actual.getPosition());
 
         // Test of undo
-        expected = Boards.WHITE_IN_MOVE_CASTLING();
+        expected = WHITE_IN_MOVE_CASTLING();
         actual.undo();
         assertEquals(expected, actual);                
     }
@@ -83,23 +95,23 @@ public class BoardTest_Castling
     public void testWhiteLongCastling_cannotCastle()
     {
         // Test of doMove
-        Board actual = new Board(Boards.WHITE_IN_MOVE_CANNOT_CASTLE());
+        Board actual = new Board(WHITE_IN_MOVE_CANNOT_CASTLE());
         boolean result = actual.doMove(Moves.WHITE_LONG_CASTLING());
         assertFalse(result);
-        assertEquals(actual, Boards.WHITE_IN_MOVE_CANNOT_CASTLE() );
+        assertEquals(actual, WHITE_IN_MOVE_CANNOT_CASTLE() );
     }
     
     @Test
     public void testBlackLongCastling()
     {
         // Test of doMove
-        Board actual = new Board(Boards.BLACK_IN_MOVE_CASTLING());
-        Board expected = Boards.AFTER_BLACK_LONG_CASTLING();        
+        Board actual = new Board(BLACK_IN_MOVE_CASTLING());
+        Board expected = AFTER_BLACK_LONG_CASTLING();
         actual.doMove(Moves.BLACK_LONG_CASTLING());
-        assertEquals(expected, actual);
+        assertEquals(expected.getPosition(), actual.getPosition());
 
         // Test of undo
-        expected = Boards.BLACK_IN_MOVE_CASTLING();
+        expected = BLACK_IN_MOVE_CASTLING();
         actual.undo();
         assertEquals(expected, actual);                
     }
@@ -109,10 +121,10 @@ public class BoardTest_Castling
     public void testBlackLongCastling_cannotCastle()
     {
         // Test of doMove
-        Board actual = new Board(Boards.BLACK_IN_MOVE_CANNOT_CASTLE());
+        Board actual = new Board(BLACK_IN_MOVE_CANNOT_CASTLE());
         boolean result = actual.doMove(Moves.BLACK_LONG_CASTLING());
         assertFalse(result);
-        assertEquals(actual, Boards.BLACK_IN_MOVE_CANNOT_CASTLE() );
+        assertEquals(actual, BLACK_IN_MOVE_CANNOT_CASTLE() );
     }
     
 }
--- src/test/java/com/simplisticjavachess/board/Boards.java
+++ src/test/java/com/simplisticjavachess/board/Boards.java
@@ -8,17 +8,7 @@ package com.simplisticjavachess.board;
 
 public class Boards
 {
-    public final static Board WHITE_IN_MOVE_CASTLING() { return Board.createFromFEN("r3k2r/8/8/8/8/8/8/R3K2R w KQkq"); }
-    public final static Board WHITE_IN_MOVE_CANNOT_CASTLE() { return Board.createFromFEN("r3k2r/8/8/8/8/8/8/R3K2R w kq"); }
-    public final static Board AFTER_WHITE_SHORT_CASTLING() {return Board.createFromFEN("r3k2r/8/8/8/8/8/8/R4RK1 b kq"); }
-    public final static Board AFTER_WHITE_LONG_CASTLING() {return Board.createFromFEN("r3k2r/8/8/8/8/8/8/2KR3R b kq"); }
 
-    
-    public final static Board BLACK_IN_MOVE_CASTLING() { return Board.createFromFEN("r3k2r/8/8/8/8/8/8/R3K2R b KQkq"); }
-    public final static Board BLACK_IN_MOVE_CANNOT_CASTLE() { return Board.createFromFEN("r3k2r/8/8/8/8/8/8/R3K2R b KQ"); }
-    public final static Board AFTER_BLACK_SHORT_CASTLING() {return Board.createFromFEN("r4rk1/8/8/8/8/8/8/R3K2R w kq"); }
-    public final static Board AFTER_BLACK_LONG_CASTLING() {return Board.createFromFEN("2kr3r/8/8/8/8/8/8/R3K2R w kq"); }
-    
     public final static Board WHITE_READY_TO_PROMOTE() { return Board.createFromFEN("8/PPPPPPPP/K/8/8/k/pppppppp/8 w"); } 
     public final static Board BLACK_READY_TO_PROMOTE() { return Board.createFromFEN("8/PPPPPPPP/K/8/8/k/pppppppp/8 b"); } 
     
