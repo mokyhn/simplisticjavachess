d7ff59c28e7039070a8b57f193ed74dbdc3d03f8
Morten Kühnrich <mkun@teknologisk.dk>
2021-01-30 20:15:06 +0100

    Updating half move clock.

--- src/main/java/com/simplisticjavachess/position/HalfMoveClock.java
+++ src/main/java/com/simplisticjavachess/position/HalfMoveClock.java
@@ -3,87 +3,67 @@ package com.simplisticjavachess.position;
 /**
  * Encapsulates dealing with half move counters
  */
-public final class HalfMoveClock
-{
+public final class HalfMoveClock {
     private final int clock;
 
-    public HalfMoveClock()
-    {
+    public HalfMoveClock() {
         this.clock = 0;
     }
 
-    public HalfMoveClock(int halfMoveClock)
-    {
-        if (halfMoveClock >= 0)
-        {
+    private HalfMoveClock(int halfMoveClock) {
+        if (halfMoveClock >= 0) {
             this.clock = halfMoveClock;
-        }
-        else
-        {
+        } else {
             throw new IllegalArgumentException();
         }
     }
 
-    public HalfMoveClock tick()
-    {
-        return new HalfMoveClock(this.clock + 1);
-    }
-
-    public int getClock()
-    {
-        return clock;
-    }
-
-    public HalfMoveClock reset()
-    {
-        return new HalfMoveClock();
-    }
-
-    public boolean isFifty()
-    {
-        return this.clock == 50;
-    }
-
-    public static HalfMoveClock fromString(String s)
-    {
-        try
-        {
+    public static HalfMoveClock fromString(String s) {
+        try {
             int clock = Integer.parseInt(s);
             return new HalfMoveClock(clock);
-        }
-        catch (NumberFormatException e)
-        {
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException("Cannot parse " + s + " which were not an integer");
         }
     }
 
+    public HalfMoveClock tick() {
+        return new HalfMoveClock(this.clock + 1);
+    }
+
+    public int getClock() {
+        return clock;
+    }
+
+    public HalfMoveClock reset() {
+        return new HalfMoveClock();
+    }
+
+    public boolean isFifty() {
+        return this.clock == 50;
+    }
+
     @Override
-	public boolean equals(Object object)
-    {
+	public boolean equals(Object object) {
         if (this == object) {
             return true;
         }
 
-        if (object instanceof HalfMoveClock)
-        {
+        if (object instanceof HalfMoveClock) {
             HalfMoveClock other = (HalfMoveClock) object;
             return this.clock == other.clock;
-        }
-        else
-        {
+        } else {
             return false;
         }
     }
 
     @Override
-    public String toString()
-    {
+    public String toString() {
         return Integer.toString(clock);
     }
 
     @Override
-    public int hashCode()
-    {
+    public int hashCode() {
         return this.clock;
     }
 
--- src/test/java/com/simplisticjavachess/position/HalfMoveClockTest.java
+++ src/test/java/com/simplisticjavachess/position/HalfMoveClockTest.java
@@ -2,21 +2,30 @@ package com.simplisticjavachess.position;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-public class HalfMoveClockTest
-{
+public class HalfMoveClockTest {
 
     @Test
-    public void testParse()
-    {
-        assertEquals(new HalfMoveClock(123), HalfMoveClock.fromString("123"));
+    public void testParse() {
+        HalfMoveClock halfMoveClock = new HalfMoveClock();
+        halfMoveClock = halfMoveClock.tick().tick();
+        assertEquals(halfMoveClock, HalfMoveClock.fromString("2"));
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testParseIll()
-    {
+    public void testParseIll() {
         HalfMoveClock.fromString("1as23");
+    }
+
+    public void testEquals() {
+        HalfMoveClock halfMoveClockA = new HalfMoveClock();
+        HalfMoveClock halfMoveClockB = new HalfMoveClock();
+        assertTrue(halfMoveClockA.equals(halfMoveClockA));
+        assertTrue(halfMoveClockA.equals(halfMoveClockB));
+        assertFalse(halfMoveClockA.equals(halfMoveClockB.tick()));
     }
 
 }
